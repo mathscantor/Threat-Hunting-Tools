0fa984ea99e74332a447039370a2c32c32a882d2
mathscantor <geraldlim619@gmail.com>
2025-07-25 15:58:52 +0800

    Added multi-threading to logs extraction

--- logs-extractor.py
+++ logs-extractor.py
@@ -9,6 +9,9 @@ import gzip
 import lzma
 import shutil
 import re
+import threading
+from collections import deque
+import time
 
 def init_logging(verbose: bool=False,
                  log_filepath: str=None) -> None:
@@ -67,7 +70,13 @@ def recursive_extraction(archive: str,
         for file in files:
             if re.search(archive_suffix_regex, file):
                 archive = os.path.join(dirpath, file)
-                recursive_extraction(archive, dirpath, True)
+                do_extract = False
+                with seen_archive_lock:
+                    if archive not in seen_archives:
+                        seen_archives.add(archive)
+                        do_extract = True
+                if do_extract:
+                    recursive_extraction(archive, dirpath, True)
     return
 
 def extract_zip(archive: str,
@@ -151,17 +160,32 @@ def extract_xz(archive: str,
         log.warning(f"Permissions error: {e}")
     return
 
-def main():
-    log.info("Starting extraction...")
-    archives = [
-        str(p) for p in pathlib.Path(args.input_dir).rglob("*")
-        if p.is_file() and re.search(archive_suffix_regex, p.name)
-    ]
-
-    log.debug(f"Found {archives}")
-    for archive in archives:
+def worker_task():
+    while (True):
+        with read_archives_head_lock:
+            log.debug(f"Remaining: {archives}")
+            if not archives:
+                break
+            archive = archives.popleft()
+            log.info(f"Recursively extracting \'{archive}\'")
         recursive_extraction(archive=archive, extract_path=args.output_dir)
-    log.info(f"Finished extraction! Please check under '{args.output_dir}'")
+    return
+
+def main():
+    start_time = time.time()
+    log.info("Starting extraction...")
+    log.debug(f"Found {archives}")
+    for _ in range(args.threads):
+        worker = threading.Thread(target=worker_task)
+        threads.append(worker)
+        worker.start()
+
+    for thread in threads:
+        thread.join()
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    log.info(f"Finished extraction in {total_time:.5f}s! Please check under '{args.output_dir}'")
     return
 
 if __name__ == "__main__":
@@ -169,9 +193,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Recursively extracts (nested) compressed logs while keeping original directory tree.')
     parser.add_argument("-i", "--input-dir", dest="input_dir", metavar="", type=str, required=True, help='The directory containing compressed logs')
     parser.add_argument("-o", "--output-dir", dest="output_dir", metavar="", type=str, required=True, help='The directory to extract to')
+    parser.add_argument("-j", "--threads", dest="threads", metavar="", type=int, required=False, help='The number of threads used for extraction (default: 1)')
     parser.add_argument("-f", '--force-overwrite', dest='force_overwrite', action='store_true', help='Force extract to an existing directory.')
     parser.add_argument("-v", '--verbose', dest='verbose', action='store_true', help='Show debug logs')
     parser.add_argument("-l", "--log-file", dest="log_file", metavar="", type=str, required=False, help='Log the progress to a file')
+    parser.set_defaults(threads=1)
     parser.set_defaults(force_overwrite=False)
     parser.set_defaults(verbose=False)
     args = parser.parse_args()
@@ -180,5 +206,14 @@ if __name__ == "__main__":
     init_logging(log_filepath=args.log_file, verbose=args.verbose)
     check_user_args()
 
+    # Globals
     archive_suffix_regex = re.compile(r'\.(zip|tar|tar\.gz|tgz|tar\.xz|txz|gz|xz)$')
+    archives = deque([
+        str(p) for p in pathlib.Path(args.input_dir).rglob("*")
+        if p.is_file() and re.search(archive_suffix_regex, p.name)
+    ])
+    seen_archives = set()
+    read_archives_head_lock = threading.Lock()
+    seen_archive_lock = threading.Lock()
+    threads = list()
     main()
